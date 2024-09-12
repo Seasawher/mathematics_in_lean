09791543862131bf31ff3d970315a2bbd2597058
Seasawher <tellagoodstory@icloud.com>
2024-09-12 21:07:06 +0900

    ゼミの進捗をコミット

--- MIL/C08_Groups_and_Rings/S01_Groups.lean
+++ MIL/C08_Groups_and_Rings/S01_Groups.lean
@@ -82,17 +82,38 @@ example {G : Type*} [Group G] (x : G) : x ∈ (⊤ : Subgroup G) := trivial
 
 example {G : Type*} [Group G] (x : G) : x ∈ (⊥ : Subgroup G) ↔ x = 1 := Subgroup.mem_bot
 
+/-- 部分群 `H ⊆ G` の `x ∈ G` による共役 `x H x⁻¹` -/
 def conjugate {G : Type*} [Group G] (x : G) (H : Subgroup G) : Subgroup G where
   carrier := {a : G | ∃ h, h ∈ H ∧ a = x * h * x⁻¹}
+
+  -- 単位元を含む
   one_mem' := by
     dsimp
-    sorry
+    use 1
+    constructor
+    · exact Subgroup.one_mem H
+    · group
+
+  -- 逆元に対して閉じている
   inv_mem' := by
     dsimp
-    sorry
+    intro y hy
+    obtain ⟨h, hmem, hy⟩ := hy
+    use h⁻¹
+    constructor
+    · exact (Subgroup.inv_mem_iff H).mpr hmem
+    · rw [hy]
+      group
+
+  -- 積を取る操作に対して閉じている
   mul_mem' := by
     dsimp
-    sorry
+    intro a b ⟨h, hmem, ha⟩ ⟨g, gmem, ga⟩
+    use h * g
+    constructor
+    · exact (Subgroup.mul_mem_cancel_right H gmem).mpr hmem
+    · rw [ha, ga]
+      group
 
 example {G H : Type*} [Group G] [Group H] (G' : Subgroup G) (f : G →* H) : Subgroup H :=
   Subgroup.map f G'
@@ -116,24 +137,43 @@ variable {G H : Type*} [Group G] [Group H]
 
 open Subgroup
 
+#synth Preorder (Subgroup G)
+#whnf (inferInstance : PartialOrder (Subgroup G))
+#synth (PartialOrder (Subgroup G))
+#synth (OmegaCompletePartialOrder (Subgroup G))
+#synth (CompleteLattice (Subgroup G))
+#whnf (inferInstance : CompleteLattice (Subgroup G))
+
 example (φ : G →* H) (S T : Subgroup H) (hST : S ≤ T) : comap φ S ≤ comap φ T := by
-  sorry
+  intro g hg
+  simp [Subgroup.mem_comap] at hg ⊢
+  exact hST hg
 
 example (φ : G →* H) (S T : Subgroup G) (hST : S ≤ T) : map φ S ≤ map φ T := by
-  sorry
+  intro h hh
+  simp [Subgroup.mem_map] at hh ⊢
+  obtain ⟨s, smem, sh⟩ := hh
+  use s
+  constructor
+  · exact hST smem
+  · assumption
 
 variable {K : Type*} [Group K]
 
 -- Remember you can use the `ext` tactic to prove an equality of subgroups.
 example (φ : G →* H) (ψ : H →* K) (U : Subgroup K) :
     comap (ψ.comp φ) U = comap φ (comap ψ U) := by
-  sorry
+  ext x
+  simp [Subgroup.mem_comap]
 
 -- Pushing a subgroup along one homomorphism and then another is equal to
 -- pushing it forward along the composite of the homomorphisms.
+-- S ⊆ G に対して
+-- ψ(φ(S)) = (ψ ∘ φ)(S)
 example (φ : G →* H) (ψ : H →* K) (S : Subgroup G) :
     map (ψ.comp φ) S = map ψ (S.map φ) := by
-  sorry
+  ext x
+  simp [Subgroup.mem_map]
 
 end exercises
 
@@ -150,17 +190,34 @@ example {G : Type*} [Group G] [Fintype G] (p : ℕ) {n : ℕ} [Fact p.Prime]
     (hdvd : p ^ n ∣ card G) : ∃ K : Subgroup G, card K = p ^ n :=
   Sylow.exists_subgroup_card_pow_prime p hdvd
 
+-- 自明な部分群であることと、要素数が１であることが同値
 lemma eq_bot_iff_card {G : Type*} [Group G] {H : Subgroup G} [Fintype H] :
     H = ⊥ ↔ card H = 1 := by
   suffices (∀ x ∈ H, x = 1) ↔ ∃ x ∈ H, ∀ a ∈ H, a = x by
     simpa [eq_bot_iff_forall, card_eq_one_iff]
-  sorry
+  constructor <;> intro h
+  · use 1
+    exact ⟨Subgroup.one_mem H, h⟩
+  · obtain ⟨x, _xmem, hx⟩ := h
+    intro y ymem
+    have := Subgroup.one_mem H
+    replace : 1 = x := by exact hx 1 this
+    rw [← this] at hx
+    exact hx y ymem
 
+-- ラグランジュの定理
 #check card_dvd_of_le
 
+-- |H| と |K| が互いに素ならば、H ∩ K = {1}
 lemma inf_bot_of_coprime {G : Type*} [Group G] (H K : Subgroup G) [Fintype H] [Fintype K]
     (h : (card H).Coprime (card K)) : H ⊓ K = ⊥ := by
-  sorry
+  rw [eq_bot_iff_card (H ⊓ K)]
+  have left : H ⊓ K ≤ H := inf_le_left
+  have right: H ⊓ K ≤ K := inf_le_right
+  replace left := card_dvd_of_le left
+  replace right := card_dvd_of_le right
+  exact Nat.eq_one_of_dvd_coprimes h left right
+
 open Equiv
 
 example {X : Type*} [Finite X] : Subgroup.closure {σ : Perm X | Perm.IsCycle σ} = ⊤ :=
@@ -170,83 +227,173 @@ example {X : Type*} [Finite X] : Subgroup.closure {σ : Perm X | Perm.IsCycle σ
 
 section FreeGroup
 
+-- S = {a, b, c}
 inductive S | a | b | c
 
 open S
 
+-- FreeGroup S := S で生成される自由群
+--             = {1, a, b, c, ab, ac, b^2, a^2 b, a b c a b c⁻¹, a b⁻¹, ...}
+-- 任意の群は、自由群の剰余群として表現できる
+
 def myElement : FreeGroup S := (.of a) * (.of b)⁻¹
+
+-- α : 集合, β : 群
+-- FreeGroup : 集合全体 Type → 群全体 Group
+-- Forget : 群全体 Group → 集合全体 Type
+--
+-- 1. 関数 f : α → Forget β
+-- 2. 群準同型 φ : FreeGroup α → β
+-- この２つの間に対応をつけることができる
+/-
+FreeGroup.lift.{u, v} {α : Type u} {β : Type v} [Group β] : (α → β) ≃ (FreeGroup α →* β)
+-/
+#check FreeGroup.lift
 
 def myMorphism : FreeGroup S →* Perm (Fin 5) :=
   FreeGroup.lift fun | .a => c[1, 2, 3]
                      | .b => c[2, 3, 1]
                      | .c => c[2, 3]
 
+#check myMorphism myElement
 
-def myGroup := PresentedGroup {.of () ^ 3} deriving Group
+-- 値は単位元で、eval するとゼロになる
+#eval myMorphism myElement
 
+-- Group でも deriving できる！？
+/-- ℤ / 3 ℤ -/
+def myGroup := PresentedGroup {.of () ^ 3}
+
+instance : Group myGroup := by
+  simp [myGroup]
+  infer_instance
+
+/-- 1 の行先だけ決めればいいので myMap : ℤ → Perm (Fin 5) だと見なせる -/
 def myMap : Unit → Perm (Fin 5)
 | () => c[1, 2, 3]
 
+/-- (1 2 3) という置換を x³ からなる式に代入すると 1 -/
 lemma compat_myMap :
     ∀ r ∈ ({.of () ^ 3} : Set (FreeGroup Unit)), FreeGroup.lift myMap r = 1 := by
   rintro _ rfl
   simp
   decide
 
+-- myMap が 3 ℤ の部分をつぶすので、商群の定義より
+-- myMap : ℤ → Perm (Fin 5) から ℤ / 3 ℤ →* Perm (Fin 5) という準同型が得られる
+-- ℤ --→ Perm (Fin 5)
+-- ↓      ↑
+-- ℤ / 3 ℤ
 def myNewMorphism : myGroup →* Perm (Fin 5) := PresentedGroup.toGroup compat_myMap
 
 end FreeGroup
 
 noncomputable section GroupActions
 
+-- 群 G があって、
+-- 群の作用 G × X → X がある
 example {G X : Type*} [Group G] [MulAction G X] (g g': G) (x : X) :
     g • (g' • x) = (g * g') • x :=
   (mul_smul g g' x).symm
 
+-- なんで v なんですか？？？
 example {G X : Type*} [AddGroup G] [AddAction G X] (g g' : G) (x : X) :
     g +ᵥ (g' +ᵥ x) = (g + g') +ᵥ x :=
   (add_vadd g g' x).symm
 
 open MulAction
 
+-- Equiv.Per X は X 上の全単射がなす群
+-- g ∈ G を、(g • ·) : X → X に対応させる。
+-- G は群なので可逆で、全単射になってる。
 example {G X : Type*} [Group G] [MulAction G X] : G →* Equiv.Perm X :=
   toPermHom G X
 
+-- Cayley の定理で出てくる奴
+-- 任意の群 G は、置換群に埋め込めるという定理があって、そこで出てくる同型。
+-- G → Equiv.Perm G という上で定義した準同型があるが、これが単射なので。
 def CayleyIsoMorphism (G : Type*) [Group G] : G ≃* (toPermHom G G).range :=
   Equiv.Perm.subgroupOfMulAction G G
 
+-- G が集合 X に作用しているときに、x ∈ X が与えられたときに、
+-- { g • x | g ∈ G} という集合が考えられて、これが x ∈ X のGによる軌道(orbit)と呼ばれる。
+--
+-- 集合 X 上の２項関係を「x ∼ y ↔ x と y の軌道が同じ」と定義するとこれは同値関係になっていて、
+-- これが多分 orbitRel。
+-- 定理の主張は、集合Xと X/∼G × orbit の間に全単射があるというもの
 example {G X : Type*} [Group G] [MulAction G X] :
     X ≃ (ω : orbitRel.Quotient G X) × (orbit G (Quotient.out' ω)) :=
   MulAction.selfEquivSigmaOrbits G X
 
+-- 上とほぼ同じ主張だと思われる。
+-- 左辺は、x ∈ X の G による軌道
+-- 右辺は、x ∈ X の安定化群
+--   ただし、x ∈ X の安定化群とは、x に作用しても変わらない g ∈ G の集合
+--   g • x = x となる g ∈ G の集合
+-- 定理の主張：
+--   左辺と右辺の間に全単射がある
+--
+-- G → orbit G x のつぶれる部分が安定化群なので
 example {G X : Type*} [Group G] [MulAction G X] (x : X) :
     orbit G x ≃ G ⧸ stabilizer G x :=
   MulAction.orbitEquivQuotientStabilizer G x
 
+-- 同型にならない例
+-- ℤ と ℤ / 2 ℤ ⊕ 2 ℤ は同型にならない
 example {G : Type*} [Group G] (H : Subgroup G) : G ≃ (G ⧸ H) × H :=
   groupEquivQuotientProdSubgroup
 
 variable {G : Type*} [Group G]
 
+theorem conjugate_mem (H : Subgroup G) (g : G) (h : H) : g * h * g⁻¹ ∈ conjugate g H := by
+  simp [conjugate]
+
 lemma conjugate_one (H : Subgroup G) : conjugate 1 H = H := by
-  sorry
+  ext x
+  constructor <;> intro h
+  case mpr =>
+    simpa [conjugate]
+  case mp =>
+    simpa [conjugate] using h
 
 instance : MulAction G (Subgroup G) where
-  smul := conjugate
+  smul g H := conjugate g H /- g • H • g⁻¹ を返す -/
   one_smul := by
-    sorry
+    intro H
+    dsimp [(· • ·)]
+    apply conjugate_one
+    done
   mul_smul := by
-    sorry
+    intro x y H
+    dsimp [(· • ·)]
+    simp [conjugate]
+    ext g
+    simp only [Set.mem_setOf_eq]
+    constructor <;> intro h
+    · obtain ⟨h, hmem, hg⟩ := h
+      use y * h * y⁻¹
+      simp [mul_left_inj, mul_right_inj, exists_eq_right', hmem, hg]
+      ac_rfl
+    · obtain ⟨v, ⟨w, wmem, hw⟩, hmem, hg⟩ := h
+      use y⁻¹ * v * y
+      constructor
+      · rw [hw]
+        simpa [mul_assoc]
+      · simp [mul_assoc]
 
 end GroupActions
 
+-- 商群
 noncomputable section QuotientGroup
 
+-- 正規部分群で割ると、商が群になる
 example {G : Type*} [Group G] (H : Subgroup G) [H.Normal] : Group (G ⧸ H) := inferInstance
 
+-- G から商群 G / H への自然な準同型がある
 example {G : Type*} [Group G] (H : Subgroup G) [H.Normal] : G →* G ⧸ H :=
   QuotientGroup.mk' H
 
+-- N → G - φ → M
 example {G : Type*} [Group G] (N : Subgroup G) [N.Normal] {M : Type*}
     [Group M] (φ : G →* M) (h : N ≤ MonoidHom.ker φ) : G ⧸ N →* M :=
   QuotientGroup.lift N φ h
@@ -263,6 +410,8 @@ example {G G': Type*} [Group G] [Group G']
 example {G : Type*} [Group G] {M N : Subgroup G} [M.Normal]
     [N.Normal] (h : M = N) : G ⧸ M ≃* G ⧸ N := QuotientGroup.quotientMulEquivOfEq h
 
+#check HEq
+
 section
 variable {G : Type*} [Group G] {H K : Subgroup G}
 
@@ -276,20 +425,144 @@ open MonoidHom
 -- The following line is working around a Lean bug that will be fixed very soon.
 attribute [-instance] Subtype.instInhabited
 
+-- |G| = |H| × |K| ならば |G / H| = |K|
 lemma aux_card_eq [Fintype G] (h' : card G = card H * card K) : card (G ⧸ H) = card K := by
-  sorry
+  have : card G = card H * H.index := by rw [Nat.mul_comm, @index_mul_card]
+  rw [this] at h'
+
+  have pos : card ↥H > 0 := card_pos
+
+  rw [← Subgroup.index_eq_card]
+  replace h' := Nat.eq_of_mul_eq_mul_left pos h'
+  assumption
+
+-- H と K は G の正規部分群
+-- H ∩ K = {1}
+-- |G| = |H| × |K|
 variable [H.Normal] [K.Normal] [Fintype G] (h : Disjoint H K) (h' : card G = card H * card K)
 
+-- 有限群に関しては、単射があって位数が等しければ同型もある
 #check bijective_iff_injective_and_card
+
+-- f が単射であることと、核が自明であることが同値
 #check ker_eq_bot_iff
+
+-- モノイド準同型をサブモノイドに制限
 #check restrict
+
+-- H ∩ K を H の部分群だと思ったもの
+#check subgroupOf
+
+-- f の制限の Ker は、f の Ker との共通部分
 #check ker_restrict
 
+open Function
+
+/-- H ∩ K = {1} で、|G| = |H| * |K| ならば、K は G / H と同型 -/
 def iso₁ [Fintype G] (h : Disjoint H K) (h' : card G = card H * card K) : K ≃* G ⧸ H := by
-  sorry
+  -- 商群への自然な準同型
+  let f : G →* G ⧸ H := QuotientGroup.mk' H
+
+  -- f の定義域を K に制限したもの
+  let φ : K →* G ⧸ H := restrict f K
+
+  -- φ が単射であることを示す
+  have inj : Injective φ := by
+    -- 核が自明であることを示せばよい
+    rw [← ker_eq_bot_iff φ]
+
+    -- φ の核を、f の核で表現する
+    rw [ker_restrict]
+
+    -- f は G から G / H への自然な準同型なので、
+    -- f の核は H に等しい
+    have : f.ker = H := QuotientGroup.ker_mk' H
+    rw [this]
+
+    -- H と K は Disjoint という仮定があるので、
+    -- H ∩ K = {1} である
+    exact subgroupOf_eq_bot.mpr h
+
+  -- φ が単射であることと、位数が等しいことから、φ は全単射である
+  have bij : Bijective φ := by
+    rw [bijective_iff_injective_and_card (f := φ)]
+
+    -- φ が単射なのはさっき示した
+    refine ⟨inj, ?_⟩
+
+    -- 位数が等しいことは仮定とさっき示した aux_card_eq から
+    have lem := aux_card_eq h'
+    rw [lem]
+
+  -- φ が全単射であることから、特に同型である
+  exact MulEquiv.ofBijective (hf := bij)
+
+-- H と K は G の正規部分群
+-- H ∩ K = {1}
+-- |G| = |H| × |K|
+variable [H.Normal] [K.Normal] [Fintype G] (h : Disjoint H K) (h' : card G = card H * card K)
+
+/-- G は G / K と G / H の直積と同型 -/
 def iso₂ : G ≃* (G ⧸ K) × (G ⧸ H) := by
-  sorry
+  -- 準同型 G → G / K
+  let fk : G →* G ⧸ K := QuotientGroup.mk' K
+
+  -- 準同型 G → G / H
+  let fh : G →* G ⧸ H := QuotientGroup.mk' H
+
+  -- 準同型 G → G / K × G / H を構成できる
+  let f : G →* (G ⧸ K) × (G ⧸ H) := MonoidHom.prod fk fh
+
+  -- f が単射であることを示す
+  have inj : Injective f := by
+    -- f の核が自明であることを示せばよい
+    rw [← ker_eq_bot_iff f]
+
+    -- f の核は、fk と fh の核の共通部分で書ける
+    rw [ker_prod]
+
+    -- fk の核と fh の核はそれぞれ K と H に等しい
+    have k_ker : fk.ker = K := QuotientGroup.ker_mk' K
+    have h_ker : fh.ker = H := QuotientGroup.ker_mk' H
+    rw [k_ker, h_ker]
+
+    -- K ∩ H は仮定から自明になる
+    exact disjoint_iff.mp (_root_.id (Disjoint.symm h))
+
+  -- f の単射性と、始域と終域の位数が等しいことから、f は全単射になる
+  have bij : Bijective f := by
+    rw [bijective_iff_injective_and_card (f := f)]
+
+    -- f が単射なのはさっき示した
+    refine ⟨inj, ?_⟩
+
+    -- 直積の位数は位数の積
+    rw [card_prod]
+
+    -- 後は aux_card_eq から従う
+    have lem := aux_card_eq h'
+    have h'' : card G = card ↥K * card ↥H := by rw [Nat.mul_comm, h']
+    have lem' := aux_card_eq h''
+    rw [lem, lem']
+    assumption
+
+  -- f が全単射であることから、特に同型である
+  exact MulEquiv.ofBijective (hf := bij)
+
+-- 直積は同型を保つ。つまり、
+-- M ≅* M' かつ N ≅* N' ならば M × N ≅* M' × N'
 #check MulEquiv.prodCongr
 
-def finalIso : G ≃* H × K :=
-  sorry
+def finalIso : G ≃* H × K := by
+  -- iso₁ から従うことをまとめておく
+  have lemH : K ≃* G ⧸ H := iso₁ h h'
+  have lemK : H ≃* G ⧸ K := iso₁ (Disjoint.symm h) (by rw [Nat.mul_comm, h'])
+
+  -- iso₂ から、G ≅* G / K × G / H が得られる
+  have lem : G ≃* (G ⧸ K) × (G ⧸ H) := iso₂ h h'
+
+  -- 後は直積が同型を保つことから示すことができる
+  have := MulEquiv.prodCongr lemK lemH
+
+  -- 同型が推移的かつ対称的であることから従う
+  exact MulEquiv.trans lem this.symm
--- MIL/C08_Groups_and_Rings/S02_Rings.lean
+++ MIL/C08_Groups_and_Rings/S02_Rings.lean
@@ -56,10 +56,13 @@ example : I * J ≤ I ⊓ J := Ideal.mul_le_inf
 
 end
 
+/-- f : R → S が環準同型で f(I) ⊆ J ならば、
+商へのリフト R / I → S / J がある -/
 example {R S : Type*} [CommRing R] [CommRing S] (I : Ideal R) (J : Ideal S) (f : R →+* S)
     (H : I ≤ Ideal.comap f J) : R ⧸ I →+* S ⧸ J :=
   Ideal.quotientMap J f H
 
+/-- I = J ならば商環が等しい -/
 example {R : Type*} [CommRing R] {I J : Ideal R} (h : I = J) : R ⧸ I ≃+* R ⧸ J :=
   Ideal.quotEquivOfEq h
 
@@ -77,59 +80,161 @@ section
 variable {ι R : Type*} [CommRing R]
 open Ideal Quotient Function
 
+#check Quotient.lift
+
 #check Pi.ringHom
 #check ker_Pi_Quotient_mk
 
-/-- The homomorphism from ``R ⧸ ⨅ i, I i`` to ``Π i, R ⧸ I i`` featured in the Chinese
+#check InfSet
+
+/-- 中国式剰余定理で登場する同型写像 ``R ⧸ ⨅ i, I i ≃+* Π i, R ⧸ I i`` のこと
+
+The homomorphism from ``R ⧸ ⨅ i, I i`` to ``Π i, R ⧸ I i`` featured in the Chinese
   Remainder Theorem. -/
-def chineseMap (I : ι → Ideal R) : (R ⧸ ⨅ i, I i) →+* Π i, R ⧸ I i :=
-  sorry
+def chineseMap (I : ι → Ideal R) : (R ⧸ ⨅ i, I i) →+* Π i, R ⧸ I i := by
+  -- `R → R / Iᵢ` という自然な写像を束ねて、
+  -- 積への写像を作る。
+  let f : R →+* Π i, R ⧸ I i := Pi.ringHom fun i ↦ mk (I i)
+
+  -- `f` を使って商からの写像を作る。
+  -- TODO: Quotiont.lift が邪魔なので隠したいが…
+  apply Ideal.Quotient.lift (⨅ i, I i) f
+
+  intro a ha
+  rwa [← @RingHom.mem_ker, ker_Pi_Quotient_mk]
 
 lemma chineseMap_mk (I : ι → Ideal R) (x : R) :
-    chineseMap I (Quotient.mk _ x) = fun i : ι ↦ Ideal.Quotient.mk (I i) x :=
-  sorry
+    chineseMap I (Quotient.mk _ x) = fun i : ι ↦ Ideal.Quotient.mk (I i) x := by rfl
 
 lemma chineseMap_mk' (I : ι → Ideal R) (x : R) (i : ι) :
-    chineseMap I (mk _ x) i = mk (I i) x :=
-  sorry
+    chineseMap I (mk _ x) i = mk (I i) x := by rfl
 
+-- f : R → S を商 R / I → S に持ち上げて単射になるのは、
+-- ker f = I のときに限る。
 #check injective_lift_iff
 
 lemma chineseMap_inj (I : ι → Ideal R) : Injective (chineseMap I) := by
-  sorry
+  dsimp [chineseMap]
+  rw [injective_lift_iff]
+  rw [ker_Pi_Quotient_mk]
 
+-- a x + b y = 1 となる a,b が存在するとき、
+-- x,y は互いに素である。
 #check IsCoprime
+
+-- イデアル I,J が互いに素 <=> I + J = (1)
 #check isCoprime_iff_add
+
+-- イデアル I,J が互いに素 <=> 1 = i + j となる i ∈ I, j ∈ J が存在する。
 #check isCoprime_iff_exists
+
+-- sup で書き直したやつ
 #check isCoprime_iff_sup_eq
+
+-- codisjoint で書き直したやつ
 #check isCoprime_iff_codisjoint
 
-#check Finset.mem_insert_of_mem
-#check Finset.mem_insert_self
+-- Finset α は、α の有限部分集合の全体
+#check Finset
 
+#check Insert.insert
+
+-- `a ∈ s` ならば、`a ∈ s ∪ {b}` が成り立つ
+#check Finset.mem_insert_of_mem
+
+#check Finset.mem_insert_self
+#check IsLawfulSingleton
+
+/-- `I ⊆ R` がイデアルで、`J : ι → Ideal R` という `R` のイデアルの族があるとする。
+かつ有限集合 `s ⊆ ι` があって `j ∈ s` に対して `Jⱼ` と `I` が互いに素だとする。
+
+このとき、`I` と `⨅ j ∈ s, Jⱼ` も互いに素
+
+### s の有限性が必要であること
+`ℤ` において `I := 2 ℤ` として、`s := (奇素数全体)`, `Jₚ := p ℤ` とすると
+任意の `j ∈ s` に対して `Jⱼ` と `I` は互いに素だが、共通部分はゼロなので主張は成り立たない。
+-/
 theorem isCoprime_Inf {I : Ideal R} {J : ι → Ideal R} {s : Finset ι}
     (hf : ∀ j ∈ s, IsCoprime I (J j)) : IsCoprime I (⨅ j ∈ s, J j) := by
+  -- 任意の命題が決定可能だという公理を導入する
   classical
+
+  -- Coprime というのを I + J = (1) に変える
   simp_rw [isCoprime_iff_add] at *
+
+  -- 有限集合 `s` のサイズに対する帰納法を使う
   induction s using Finset.induction with
-  | empty =>
-      simp
+  | empty => simp
   | @insert i s _ hs =>
       rw [Finset.iInf_insert, inf_comm, one_eq_top, eq_top_iff, ← one_eq_top]
       set K := ⨅ j ∈ s, J j
       calc
-        1 = I + K                  := sorry
-        _ = I + K * (I + J i)      := sorry
-        _ = (1 + K) * I + K * J i  := sorry
-        _ ≤ I + K ⊓ J i            := sorry
+        1 = I + K := ?step1
+        _ = I + K * (I + J i)      := ?step2
+        _ = (1 + K) * I + K * J i  := ?step3
+        _ ≤ I + K ⊓ J i            := ?step4
+
+      case step1 =>
+        symm
+        apply hs
+        intro j hj
+        specialize hf j; simp [hj] at hf
+        simp_all only [Submodule.add_eq_sup, one_eq_top]
+
+      case step2 =>
+        suffices 1 = I + J i from by
+          nth_rw 1 [show K = K * 1 from by simp]
+          rw [← this]
+        specialize hf i; simp at hf
+        simp_all only [Submodule.add_eq_sup, one_eq_top]
+
+      case step3 =>
+        have lem : K * (I + J i) = K * I + K * J i := by
+          exact LeftDistribClass.left_distrib K I (J i)
+        rw [lem]
+        replace lem : (1 + K) * I = I + K * I := by
+          exact one_add_mul K I
+        rw [lem]
+        ac_rfl
+
+      -- イデアルが積で閉じていることを利用して包含を示す
+      case step4 =>
+        have lem : K * J i ≤ K ⊓ J i := by
+          exact mul_le_inf
+        suffices (1 + K) * I ≤ I from by
+          exact add_le_add this lem
+        exact mul_le_left
+
+/-- イデアルの有限族 `{ Iᵢ } (i ∈ ι)` に対して、
+相異なる添え字に対して `Iᵢ` は互いに素だとする。
+このとき、中国式剰余定理が存在を主張している写像 `R / ⨅ i, Iᵢ → ⨅ i, (R / Iᵢ)` が全射 -/
 lemma chineseMap_surj [Fintype ι] {I : ι → Ideal R}
     (hI : ∀ i j, i ≠ j → IsCoprime (I i) (I j)) : Surjective (chineseMap I) := by
   classical
+  -- 値域から何か元が与えられたとする
   intro g
+
+  -- 各 `R → R / Iᵢ` は全射なので、
+  -- 任意の `i : ι` に対して、`g` の射影と一致する元 `f i` を選んでくることができる
   choose f hf using fun i ↦ Ideal.Quotient.mk_surjective (g i)
+
+  -- 任意の `i` に対して、
+  -- ある `eᵢ : R` が存在して、`eᵢ in (R / I i) = 1` で、
+  -- 他の `j` に対しては `eᵢ in (R / I j) = 0` を満たす
   have key : ∀ i, ∃ e : R, mk (I i) e = 1 ∧ ∀ j, j ≠ i → mk (I j) e = 0 := by
     intro i
     have hI' : ∀ j ∈ ({i} : Finset ι)ᶜ, IsCoprime (I i) (I j) := by
+      intro j hj
+      specialize hI i j
+      apply hI
+      simp at hj ⊢
+      exact fun a ↦ hj (id (Eq.symm a))
+    replace hI' := isCoprime_Inf hI'
+    clear * - hI'
+    replace hI' : (I i) + (⨅ j ∈ ({i} : Finset ι)ᶜ, I j) = 1 := by
+      exact IsCoprime.add_eq hI'
+    set K := ⨅ j ∈ ({i} : Finset ι)ᶜ, I j with hK
+    replace hI' : ∃ x ∈ I i, ∃ y ∈ K, x + y = 1 := by
       sorry
     sorry
   choose e he using key
